09f3bf509ca40b3b2f5b8ef2fa9d7a0edaa504a6
Nathan Helmly <nathan.helmly@gmail.com>
2021-03-21 11:24:29 -0400

    commit

--- src/Chapter1.gd
+++ src/Chapter1.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
 
-func _init():
-	$plr/Cutscene/Anim.play("BeginClip")
-
-
 func _on_Anim_finished(anim_name):
 	Debugger.AddToHistory(" unhandled exception \"\" ", Debugger.TALKER.Error)
 	GameState.SetStep(10)
 	GameState.LoadScene(Enums.scene.Splash)
+
+func _on_Timer_timeout():
+	$plr/Cutscene/Anim.play("BeginClip")
